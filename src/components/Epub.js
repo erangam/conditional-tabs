3cdfa309c7f8bf4cec6c2bd81c3f26650ff3d66a
David O'Dey <david.odey@pearson.com>
2017-11-13 10:18:36 -0700

    added page one feldman

---
+++ src/components/Epub.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Epub extends Component {
+	render() {
+		return (
+			<div className="home">
+				<header className="App-header">
+					<h1 className="pe-page-title">Feldman Epub</h1>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default Epub;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../components/Home';
+import Epub from '../components/Epub';
 
 export default () => {
 	return (
 		<BrowserRouter>
 			<Switch>
 				<Route path="/" exact component={Home} />
+				<Route path="/epub" exact component={Epub} />
 			</Switch>
 		</BrowserRouter>
 	);
