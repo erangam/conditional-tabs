ecf6198767e55a2befb6b3577814c271eeaaf19d
David O'Dey <david.odey@pearson.com>
2017-11-13 17:05:58 -0700

    added new pathname based on env

--- src/routes/feldman.js
+++ src/routes/feldman.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PageOne from '../epubs/feldman/chapter-eight/PageOne';
 import PageTwo from '../epubs/feldman/chapter-eight/PageTwo';
+const baseUrl = process.env.PUBLIC_URL;
 
 const FeldmanRoute = props => {
 	return (
@@ -10,13 +11,13 @@ const FeldmanRoute = props => {
 			<Switch>
 				<Route
 					name="one"
-					path="/chapter-eight/one"
+					path={baseUrl + '/chapter-eight/one'}
 					exact
 					render={() => <PageOne path={props.match} />}
 				/>
 				<Route
 					name="two"
-					path="/chapter-eight/two"
+					path={baseUrl + '/chapter-eight/two'}
 					exact
 					render={() => <PageTwo path={props.match} />}
 				/>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../components/Home';
 import Feldman from '../epubs/feldman/Feldman';
+const baseUrl = process.env.PUBLIC_URL;
 
 const MainRoute = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/feldman" component={Feldman} />
+				<Route path={baseUrl + '/'} exact component={Home} />
+				<Route path={baseUrl + '/feldman'} component={Feldman} />
 			</Switch>
 		</BrowserRouter>
 	);
