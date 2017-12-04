41b04cb8f87f943d4d3145c299d70ea5dd99275b
David O'Dey <david.odey@pearson.com>
2017-12-04 13:56:19 -0700

    fixed a simple className error where class was being used insted

--- src/epubs/feldman/chapter-eight/Page_3.js
+++ src/epubs/feldman/chapter-eight/Page_3.js
@@ -9,26 +9,26 @@ const PageTwo = props => {
 				<section
 					data-chaucer-element-id="f30ddf1d-ffde-4ba7-ba65-4762fb9d08a5"
 					id="P7001013171000000000000000003CFB"
-					class="level1"
+					className="level1"
 					data-chaucer-note-id="329187"
 				>
 					<header data-chaucer-element-id="45908a7d-07b2-47f8-b0f9-9758fa2c5875">
 						<h1
 							data-chaucer-element-id="7d56cc29-97ef-4455-a5c3-1ebaa5300aee"
-							class="title"
+							className="title"
 							data-uri="M08_FELD7654_04_SE_C08.xhtml#P7001013171000000000000000013B30"
 							srcLang="title"
 						>
 							Physical Development and Sexuality
 						</h1>
 					</header>
-					<section class="objectiveset">
-						<ol class="objectivelist dynamiclist">
-							<li class="objective">
+					<section className="objectiveset">
+						<ol className="objectivelist dynamiclist">
+							<li className="objective">
 								<p>
-									<span class="lc_labelnum">
-										<span class="label">LO</span>{' '}
-										<span class="number">8.1</span>
+									<span className="lc_labelnum">
+										<span className="label">LO</span>{' '}
+										<span className="number">8.1</span>
 									</span>{' '}
 									Analyze physical development in middle adulthood.
 								</p>
@@ -39,7 +39,7 @@ const PageTwo = props => {
 					<aside
 						data-chaucer-element-id="55cbb5a4-cf62-4a30-9f6a-086cc2d0c683"
 						id="P7001013171000000000000000003CFD"
-						class="vignette"
+						className="vignette"
 						data-uri="M08_FELD7654_04_SE_C08.xhtml#P7001013171000000000000000003CFD"
 					>
 						<p
