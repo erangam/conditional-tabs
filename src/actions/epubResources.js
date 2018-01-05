ea9c6fa7df49cab5dc2db3efb1c94e27e15561fe
David O'Dey <david.odey@pearson.com>
2018-01-05 13:59:46 -0700

    fixed build

--- src/actions/epubResources.js
+++ src/actions/epubResources.js
@@ -3,8 +3,6 @@ import resources from '../epubs/feldman/resources/bm01_pg0002.xhtml';
 import glossary from '../epubs/feldman/resources/bm01_pg0001.xhtml';
 export const FETCH_REFERENCE_HTML = 'FETCH_REFERENCE_HTML';
 export const FETCH_GLOSSARY_HTML = 'FETCH_GLOSSARY_HTML';
-const baseUrl = process.env.PUBLIC_URL;
-// change this url to the api of your choice. Data will bind to a prop called 'apiData' within the redux store.
 
 export function getReferenceHtml() {
 	const request = axios.get(resources);
