ffb66b59bb852f417fc408850cdd605d876c969e
David O'Dey <david.odey@pearson.com>
2018-01-05 09:57:12 -0700

    quick change

--- src/actions/epubResources.js
+++ src/actions/epubResources.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import resources from '../epubs/feldman/resources/bm01_pg0002.xhtml';
-import glossary from '../epubs/feldman/resources/bm01_pg0001.xhtml';
+import resources from './epubs/feldman/resources/bm01_pg0002.xhtml';
+import glossary from './epubs/feldman/resources/bm01_pg0001.xhtml';
 export const FETCH_REFERENCE_HTML = 'FETCH_REFERENCE_HTML';
 export const FETCH_GLOSSARY_HTML = 'FETCH_GLOSSARY_HTML';
 const baseUrl = process.env.PUBLIC_URL;
