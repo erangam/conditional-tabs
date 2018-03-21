d52fdfa60a85f71f3207f4c7acf29f0ca8df0a89
David O'Dey <dodey@icloud.com>
2018-03-21 14:03:59 -0700

    fixed travis builds

--- src/epubs/RevelFooter.js
+++ src/epubs/RevelFooter.js
@@ -1,10 +1,10 @@
 // Stateless component boilerplate
-import React, {Fragment} from "react";
+import React from "react";
 const RevelFooter = (props) => {
 	if (props.display === true) {
 		return (
 			<div className="revel-footer">
-				<button className="pe-icon--btn" aria-label>
+				<button className="pe-icon--btn">
 					<svg focusable="false" className="pe-icon--view-list-18" aria-hidden="false" aria-label="book seats" title="view list" role="img">
 						<use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#view-list-18" />
 					</svg>
@@ -12,7 +12,7 @@ const RevelFooter = (props) => {
 				<div className="slidecontainer">
 					<input type="range" min={1} max={100} defaultValue={50} className="slider" id="myRange" />
 				</div>
-				<button className="pe-icon--btn" aria-label>
+				<button className="pe-icon--btn">
 					<svg focusable="false" className="pe-icon--font-setting-18" aria-hidden="false" aria-label="set font size" title="book seats" role="img">
 						<use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#font-setting-18" />
 					</svg>
--- src/epubs/RevelHeader.js
+++ src/epubs/RevelHeader.js
@@ -1,12 +1,12 @@
 // Stateless component boilerplate
-import React, {Fragment} from "react";
+import React from "react";
 import menu from 'material-design-icons/navigation/svg/design/ic_menu_24px.svg'
 const RevelHeader = (props) => {
 	if (props.display === true ) {
 		return (
 			<div className="revel-header" style={{zIndex: "100"}}>
 				<button className="pe-icon--btn">
-					<img src={menu} />
+					<img src={menu} alt={"toc menu"}/>
 				</button>
 			</div>
 		);
